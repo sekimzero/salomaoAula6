867d79d1dc8649ab67c0f25729fbfa3bd568d15d
salomao kim <sekim.maua@gmail.com>
2022-06-12 07:34:09 -0300

    resolvido mesa de trabalho

--- salomaoAula6-Mesa/Program.cs
+++ salomaoAula6-Mesa/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+
+string nomeDoUsuario;
+int idadeDoUsuario;
+int anoAtual = 2022;
+int anoDeNascimento;
+
+
+
+Console.WriteLine("digite o seu nome:");
+nomeDoUsuario = Console.ReadLine();
+
+Console.WriteLine("digite sua idade:");
+idadeDoUsuario = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("digite o ano atual:");
+//anoAtual = int.Parse(Console.ReadLine());
+anoAtual = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine($"Ola {nomeDoUsuario}, voce nasceu em {anoAtual - idadeDoUsuario}.");
+
