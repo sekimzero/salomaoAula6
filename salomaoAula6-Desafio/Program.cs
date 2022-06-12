0597e4c610b761cd26fae3a26fd63e8021a6472d
salomao kim <sekim.maua@gmail.com>
2022-06-12 07:35:23 -0300

    resolvido desafio do professor

--- salomaoAula6-Desafio/Program.cs
+++ salomaoAula6-Desafio/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+double quilometrosPercorridosPeloVeiculo;
+Console.WriteLine("quantos quilometros voce percorreu?");
+quilometrosPercorridosPeloVeiculo = double.Parse(Console.ReadLine());
+double distanciaPercorrida;
+distanciaPercorrida = quilometrosPercorridosPeloVeiculo * 0.621371;
+Console.WriteLine($"distanciaPercorrida eh de {distanciaPercorrida} milhas");
+
+
+
